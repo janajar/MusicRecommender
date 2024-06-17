caab2389637b0619f6ecdb3199309179bcaee471
janajar <113301432+janajar@users.noreply.github.com>
2024-06-17 19:27:00 -0400

    Styling For Website
    Basic background and header styles to give rough idea of
    the UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Header from './Header.js'
+import './App.css'
 
 function App() {
   return (
-    <div>
-
+    <div className='background'>
+      <Header/>
     </div>
-  )
+  );
 }
-
 export default App
---
+++ client/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './img/logo.svg'
+import './Header.css'
+function Header() {
+  return (
+    <header className='header'>
+      <img src={logo} alt="TikTok Studio Logo" style={{ height: '50px', marginRight: '10px' }} />
+      <h1>
+        TikTok Studio
+      </h1>
+    </header>
+  );
+}
+
+export default Header;

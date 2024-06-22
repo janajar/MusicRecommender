9e79fc942a368ada6c3400559fb715b25fd824d6
janajar <113301432+janajar@users.noreply.github.com>
2024-06-22 14:37:08 -0400

    Embedded Video Uploader
    Allows for videos to be uploaded and a preview to be shown, but
    still very buggy. Uploading component does not disappear after
    uploading. Video does not shift to the correct place. Just temp
    code, most likely will refactor.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header.js'
+import Uploader from './Uploader.js'
 import './App.css'
 
 function App() {
   return (
     <div className='background'>
       <Header/>
+      <Uploader/>
     </div>
   );
 }

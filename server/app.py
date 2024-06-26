8da809708804361e76ef39ec28cc30af86640b54
janajar <113301432+janajar@users.noreply.github.com>
2024-06-26 00:11:37 -0400

    Personalized Recommendations
    Returns the top 10 recommendations based on generated hashtags for
    a given video

--- server/app.py
+++ server/app.py
@@ -1,43 +1,24 @@
 from flask import Flask, request, jsonify
-from scrape import scrape_tiktok, popularity_score
-from aiohttp import ClientSession
-import asyncio
+from scrape import personalized_recommendations
+
 app = Flask(__name__)
-
-
-async def get_ms_token():
-    async with ClientSession() as session:
-        async with session.get("https://www.tiktok.com/") as response:
-            cookies = response.cookies
-            ms_token = cookies.get('msToken') if 'msToken' in cookies else None
-            return ms_token
 
 
 @app.route('/')
 @app.route('/recommend-music', methods=['POST'])       
 def recommend_music():
-    # data = request.get_json()
-    # hashtags = data.get('hashtags', [])
-    hashtags = ["gojo"]
+    data = request.get_json()
+    hashtags = data.get('hashtags', [])
+    
     if not isinstance(hashtags, list):
         return jsonify({"error": "Hashtags must be a list"}), 400
     
-    #try:
-    recommendations = []
-    ms_token = asyncio.run(get_ms_token())
-    for hashtag in hashtags:
-        results = asyncio.run(scrape_tiktok(hashtag, ms_token))
-        recommendations.append(results)
-    # return jsonify({"sounds": str(recommendations[0].items())})
-    recommendations = sorted(recommendations, key=popularity_score)
-
-    sounds = []
-    for recommendation in recommendations:
-        sounds.append((recommendation[1].title, recommendation[1].original))
-
-    return jsonify({"sounds": sounds, "size": len(recommendations)})
-    # except Exception as e:
-    #     return jsonify({"error": str(e)}), 500
+    try:
+        personalized = personalized_recommendations(hashtags)
+        return jsonify({"personalized": personalized, "trending": "TODO", "new": "TODO"})
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
--- server/scrape.py
+++ server/scrape.py
@@ -1,18 +1,47 @@
 from TikTokApi import TikTokApi
 from math import log
-async def scrape_tiktok(hashtag, ms_token):
+import asyncio
+from aiohttp import ClientSession
+
+def personalized_recommendations(hashtags):
+    recommendations = []
+    ms_token = asyncio.run(get_ms_token())
+    for hashtag in hashtags:
+        results = asyncio.run(hashtag_videos(hashtag, ms_token))
+        recommendations.extend(results)
+
+    recommendations = sorted(recommendations, key=popularity_score)
+    sounds = []
+    for recommendation in recommendations:
+        if hasattr(recommendation.sound, 'play_url'):
+            author = recommendation.author.username if not hasattr(recommendation.sound, 'author') else recommendation.sound.author
+            sounds.append({"title":recommendation.sound.title, "author":author, "url":recommendation.sound.play_url})
+
+        if len(sounds) == 10:
+            break
+    return sounds
+
+async def get_ms_token():
+    async with ClientSession() as session:
+        async with session.get("https://www.tiktok.com/") as response:
+            cookies = response.cookies
+            ms_token = cookies.get('msToken') if 'msToken' in cookies else None
+            return ms_token
+
+async def hashtag_videos(hashtag, ms_token):
     async with TikTokApi() as api:
         await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless = False)
         tag = api.hashtag(name=hashtag)
+        videos = []
         async for video in tag.videos(count=30):
-            return [video.stats, video.sound]
+            videos.append(video)
+        return videos 
 
 def popularity_score(video):
-    score = 0.35 * log(video[0]["playCount"] + 1) 
-    + 0.25 * log(video[0]["diggCount"] + 1)
-    + 0.20 * log(video[0]["shareCount"] + 1) 
-    + 0.15 * log(video[0]["commentCount"] + 1)
-    + 0.05 * log(video[0]["collectCount"] + 1)
+    score = 0.35 * log(video.stats["playCount"] + 1) 
+    + 0.25 * log(video.stats["diggCount"] + 1)
+    + 0.20 * log(video.stats["shareCount"] + 1) 
+    + 0.15 * log(video.stats["commentCount"] + 1)
+    + 0.05 * log(video.stats["collectCount"] + 1)
 
     return score
-
